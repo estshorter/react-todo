d84060fd5751a99a6ff2602cc4bc3eb826b52d06
estshorter <1430311+estshorter@users.noreply.github.com>
2020-05-22 22:15:42 +0900

    Updates

---
+++ precache-manifest.5e6d5a3c9ab4fe08e7a63f508648dce0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50dc090423576ecc1ed9b67b93573cc4",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "2474cbbac3b544be7a79",
+    "url": "/react-todo/static/css/main.b6c5f04a.chunk.css"
+  },
+  {
+    "revision": "7ba5a194c99b47ede3b7",
+    "url": "/react-todo/static/js/2.2bc24df1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.2bc24df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2474cbbac3b544be7a79",
+    "url": "/react-todo/static/js/main.c507b797.chunk.js"
+  },
+  {
+    "revision": "073aed0f486fa2db3702",
+    "url": "/react-todo/static/js/runtime-main.1a3e2e23.js"
+  }
+]);
